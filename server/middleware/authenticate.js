4c2238108e04785055dbd9a8afc7533e5b5d4cbd
Sindrip <sindripetur@gmail.com>
2018-02-14 22:15:29 +0000

    is consolelog magic

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,4 +1,6 @@
 const authenticate = (req, res, next) => {
+    console.log(req.session);
+    console.log('userid: ', req.session.user_id);
     if (req.session && req.session.user_id) {
         next();
     } else {
