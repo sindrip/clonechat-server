deac9e0a9c0314f5eaec110eb110439303d74256
Sindrip <sindripetur@gmail.com>
2018-02-14 22:45:44 +0000

    cookies are funny on android

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,6 +1,4 @@
 const authenticate = (req, res, next) => {
-    console.log(req.session);
-    console.log('userid: ', req.session.user_id);
     if (req.session && req.session.user_id) {
         next();
     } else {
